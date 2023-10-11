80a3caefee68565136a31bdf2441e8029d4ca909
Max <maksis1701@mail.ru>
2023-10-11 18:42:33 +0600

    practise 3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import Service.ServiceD;
 
 public class Main {
     public static void main(String [] args) {
-        ServiceD serviceD = new ServiceD();
-        serviceD.methodD();
-        System.out.println(serviceD);
+
     }
 }
